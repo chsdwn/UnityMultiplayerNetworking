9e647a285b1cb9e1118e65c34d7aae4aac189bc1
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-11-27 23:27:40 +0300

    4 - Network Manager

---
+++ MultiplayerBasics/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("Connected to the server");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log("A Player Joined");
+        Debug.Log($"{numPlayers} {(numPlayers > 1 ? "players" : "player")} connected");
+    }
+}
